2fca8de065a1acb65d2222eb1148e16495d1191b
Frank Greguska <francis.greguska@jpl.nasa.gov>
2016-02-16 11:42:36 -0800

    Initial

---
+++ src/main/java/com/example/ApplicationA.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Created by greguska on 2/16/16.
+ */
+@SpringBootApplication
+@ImportResource("classpath:run-app-a-context.xml")
+public class ApplicationA {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ApplicationA.class, args);
+
+        System.out.println(context.getBean("helloA"));
+        System.out.println(context.getBean("helloB"));
+    }
+}
